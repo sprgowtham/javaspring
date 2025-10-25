37ed77dad8b112a449339a98f789f7067fcd11a6
sprgowtham <sprgowtham@gmail.com>
2025-10-25 14:57:19 -0400

    Initial commit with project files

--- my-spring-project/src/main/java/com/example/MySpringProjectApplication.java
+++ my-spring-project/src/main/java/com/example/MySpringProjectApplication.java
@@ -1,10 +1,23 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MySpringProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MySpringProjectApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+class SampleController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Gowtham!";
     }
 }
